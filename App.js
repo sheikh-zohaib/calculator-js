37496dc8e6620e3d38cb9cfd70fe7a698404ffa4
Mahummad zohaib <shahgamerz798@gmail.com>
2026-09-18 06:47:02 +0500

    Implement basic calculator functionality in App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+let screen = document.getElementById("screen");
+function calc(val){
+  if(val == "C"){
+    screen.value = "";
+  }
+  else if(val == "="){
+    try{
+      screen.value = eval(screen.value);
+    }catch{
+      screen.value = "Error";
+    }
+  }
+  else{
+    screen.value += val;
+  }
+}
